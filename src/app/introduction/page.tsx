1cb36a77e031c8f596aa79af672cbea7567fe9e3
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-04 22:38:23 +1100

    Added paragraph tags

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -1,8 +1,23 @@
 export default function IntroductionPage() {
     return (
         <>
-        <h1>Character Page</h1>
-        <p>K is an original animation series filled with mysteries upon mysteries. </p>
+        <h1>INTRODUCTION</h1>
+        <p>K is an original animation series filled with mysteries upon mysteries. The story is set 
+            in a slightly different modern day Japan, where 7 Kings each have their own feuds and alliances.
+            A young highschool student somehow finds himself involved in a battle between people with unbelievable 
+            powers. The story is created by the GoRA, 7 masked authors, who have carefully drawn up a story that includes
+            the unique writing ability of each of the GoRA members. The animation is done by 'GoHands', the same studio behind
+            Mardock Scramble. The show is directed and features character designs by Shingo Suzuki, and features music from
+            Mikio Endo (Guilty Honey) and animation direction by Makoto Furuta (Seitokai Yakuindomo) - a truly amazing group of staff!
+        </p>
+        <p>
+            The cast of the show features many popular voice actors including Daisuke Namikawa, Daisuke Ono, Mikako Komatsu,
+            Kenjiro Tsuda, and Tomokazu Sugita. The opening theme is sung by Angela and ending by Mikako Komatsu, both of which
+            are attracting lots of attention. Together with the start of the broadcast, a manga version of the show by Yui Kuroe
+            will be serialised in Kodansha ARIA - the manga will be titled 'K -Memory of Red-' and will tell the prelude of the anime's
+            story line. Additionally, from Novemeber the novel based around the series, created by GoRA, will be available in three volumes.
+            The series will be available over a number of media for fans to enjoy.
+        </p>
         </>
     )
 }
