6ae02540d266e9e3390f717440374aefd9aaf5b4
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-06 19:41:59 +1100

    add caching to the route

--- src/app/api/character/route.tsx
+++ src/app/api/character/route.tsx
@@ -1,20 +1,23 @@
 import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-static';
 
 export async function GET() {
     try {
-        const file = await fs.readFile(process.cwd() + '/src/app/data/character.json', 'utf8');
+        const file = await fs.readFile(
+            process.cwd() + '/src/app/data/character.json',
+            'utf8'
+        );
         console.log(file);
-    
+
         const data = JSON.parse(file);
         console.log(data);
-    
+
         return NextResponse.json(data);
-    } catch(error) {
+    } catch (error) {
         return NextResponse.json(
-            { error: "Internal Server Error" },
+            { error: 'Internal Server Error' },
             { status: 500 }
         );
-
     }
 }
