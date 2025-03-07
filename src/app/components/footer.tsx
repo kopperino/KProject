c2670d56f781de9e008dc09c5fc92d97791e7c9a
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-07 23:13:26 +1100

    Added footer to placeholder

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,75 @@
+export default function Footer() {
+    return (
+        <div className="bg-[#F5F5F5] text-center p-10 text-2xl font-bold flex justify-between">
+            <div>
+                <div>K</div>
+                <div>K</div>
+            </div>
+            <div className="grid grid-cols-1">
+                <div>Test</div>
+                <div>Test</div>
+                <div>Test</div>
+            </div>
+
+            <div className="grid grid-cols-1 gap-4">
+                <a
+                    href="/about"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    About Us
+                </a>
+                <a
+                    href="/contact"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Contact
+                </a>
+                <a
+                    href="/privacy-policy"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Privacy Policy
+                </a>
+                <a
+                    href="/terms"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Terms & Conditions
+                </a>
+                <a href="/blog" className="text-[#2DAA9E] hover:text-[#229799]">
+                    Blog
+                </a>
+            </div>
+
+            <div className="grid grid-cols-1 gap-4">
+                <a
+                    href="/about"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    About Us
+                </a>
+                <a
+                    href="/contact"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Contact
+                </a>
+                <a
+                    href="/privacy-policy"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Privacy Policy
+                </a>
+                <a
+                    href="/terms"
+                    className="text-[#2DAA9E] hover:text-[#229799]"
+                >
+                    Terms & Conditions
+                </a>
+                <a href="/blog" className="text-[#2DAA9E] hover:text-[#229799]">
+                    Blog
+                </a>
+            </div>
+        </div>
+    );
+}
