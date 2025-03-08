1d296561592d3d0c54aeb625dcf339f65aa49b48
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-09 09:18:59 +1100

    Added mobile responsiveness to the page!

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -10,7 +10,9 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
-import { Speech } from 'lucide-react';
+import { Calendar, Ruler, Speech } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+import Link from 'next/link';
 
 type Character = {
     id: number;
@@ -21,6 +23,7 @@ type Character = {
     description: string;
     quote: string;
     mugshot: string;
+    myanimelist: string | null;
 };
 
 async function getData() {
@@ -44,11 +47,13 @@ export default async function CharacterPage() {
                 {data.map((character) => (
                     <Card key={character.id} className="hover:bg-amber-100">
                         <CardHeader>
-                            <CardTitle>{character.name}</CardTitle>
+                            <CardTitle className="font-bold text-md md:text-2xl">
+                                {character.name}
+                            </CardTitle>
                         </CardHeader>
 
-                        <CardContent className="grid grid-cols-2 place-items-center">
-                            <div>
+                        <CardContent className="grid grid-cols-1 md:grid-cols-2">
+                            <div className="max-h-32 overflow-auto">
                                 <CardDescription className="text-md">
                                     {character.description}
                                 </CardDescription>
@@ -56,21 +61,41 @@ export default async function CharacterPage() {
                                     "{character.quote}"
                                 </CardDescription>
                             </div>
-                            <div>
+                            <div className="mt-10 md:mt-0">
                                 <Image
                                     src={character.mugshot}
                                     width={150}
                                     height={150}
                                     alt={character.mugshot}
-                                    className="shadow-xl rounded-xl"
+                                    className="shadow-xl rounded-xl mx-auto"
                                 />
+                                <div className="flex flex-wrap justify-center gap-2 mt-5 flex-col sm:flex-row place-items-center">
+                                    <div className="flex gap-2">
+                                        <Badge className="px-5 py-1">
+                                            <Calendar />
+                                            <p>{character.age}</p>
+                                        </Badge>
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <Badge className="px-2 py-1">
+                                            <Ruler />
+                                            <p>{character.height}cm</p>
+                                        </Badge>
+                                    </div>
+                                </div>
                             </div>
                         </CardContent>
-                        <CardFooter>
-                            <p className="flex gap-2 bg-amber-100 rounded-4xl px-3 py-2">
-                                <Speech /> {character.voice_actor}
-                            </p>
-                        </CardFooter>
+                        <Link
+                            href={character.myanimelist ?? '#'}
+                            target="_blank"
+                        >
+                            <CardFooter>
+                                <p className="flex justify-center sm:justify-start gap-2 bg-amber-100 rounded-4xl px-3 py-2 border-2 border-black transition-transform duration-200 ease-in-out transform hover:scale-105 text-sm mx-auto truncate md:text-md md:ml-0 min-w-[150px]">
+                                    <Speech className="hidden md:block" />{' '}
+                                    {character.voice_actor}
+                                </p>
+                            </CardFooter>
+                        </Link>
                     </Card>
                 ))}
             </div>
