64592f9a0d03f5e2215a2a09ed68b25ea0c0dbdd
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-09 21:24:19 +1100

    site index updated for update to do with hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import GridSection from './components/grid-section';
 import NavBar from './components/navbar';
+import Hero from './components/ultimate-hero';
 
 export default function Home() {
     return (
         <>
             <NavBar />
+            <Hero />
+            <GridSection />
         </>
     );
 }
